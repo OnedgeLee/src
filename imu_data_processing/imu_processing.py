c82ebb8f517c48e214c679a3a3869aa644dca22b
shetshield <shetshield@gmail.com>
2021-02-06 12:21:17 +0900

    Add files via upload

--- imu_data_processing/imu_processing.py
+++ imu_data_processing/imu_processing.py
@@ -1,94 +1,111 @@
 #%%
 import serial, time
-import numpy as np
+import matplotlib.pyplot as plt
+from math import degrees, atan
 
-def raw_to_data(_raw) :
-    '''
-        # _raw: raw data(Euler Angle) from IMU
-    '''
-    _index = list()
-    _raw = _raw.rstrip("\\r\\n")
-    for i in range(5, len(_raw)) :
-        _temp = _raw[i]
-        if _temp == ',' :
-            _index.append(i)
-    _roll  = float(_raw[_index[0]+1:_index[1]])
-    _pitch = float(_raw[_index[1]+1:_index[2]])
-    _yaw   = float(_raw[_index[2]+1:_index[3]])
-    _ax    = float(_raw[_index[3]+1:_index[4]])
-    _ay    = float(_raw[_index[4]+1:_index[5]])
-    _az    = float(_raw[_index[5]+1:len(_raw)-2])
-    return [_roll, _pitch, _yaw], [_ax, _ay, _az]
+mv_avg   = 30
+time_thr = 0.004
+r  = 1
+p  = 2
+y  = 3
+ax = 4
+ay = 5
+az = 6
+
+class ReadLine:
+    def __init__(self, s) :
+        self.buf = bytearray()
+        self.s   = s
+        
+    def readline(self) :
+        i = self.buf.find(b'\n')
+        if i >= 0 :
+            r = self.buf[:i+1]
+            self.buf = self.buf[i+1:]
+            return r
+        while True :
+            i = max(1, min(2048, self.s.in_waiting))
+            data = self.s.read(i)
+            i = data.find(b'\n')
+            
+            if i >= 0 :
+                r = self.buf + data[:i+1]
+                self.buf[0:] = data[i+1:]
+                return r
+            else :
+                self.buf.extend(data)
+
+
+def raw_parsing(_raw) :
+    # print(_raw)
+    _parsed = _raw.decode().split(',')
+    return _parsed
+
+def ser_init() :
+    _ser = serial.Serial(port='COM5', baudrate=921600)
+    _ser.close()
+    time.sleep(0.3)
+    _ser.open()
+    time.sleep(0.3)
+    return _ser
+
+def angle_init(_rl, _mvavg) :
+    time.sleep(1)
+    # _r, _p, _y = 0, 0, 0
+    _r, _p = 0, 0
+    while True :
+        if len(_rl.readline()) > 0 :
+            break
+    for _ in range(_mvavg) :
+        _parsed = raw_parsing(_rl.readline())
+        _r += float(_parsed[r])
+        _p += float(_parsed[p])
+        # _y += float(_parsed[y])
+    _r = _r/_mvavg
+    _p = _p/_mvavg
+    # _y = _y/_mvavg
+    # return _r, _p, _y
+    return _r, _p
 
 def main() :
-    serImu = serial.Serial(port='COM11', baudrate=921600, timeout=0.001)
-    serImu.close()
-    time.sleep(0.5)
-    serImu.open()
-    time.sleep(0.5)
+    ser = ser_init()
+    rl  = ReadLine(ser)
+    r0, p0 = angle_init(rl, mv_avg)
+    # r0, p0, y0 = angle_init(rl, mv_avg)
+    # print("r, p init value", r0, p0)
+    R0, P0 = r0, p0
+    t0 = time.time()
+    while True :
+        try :
+            raw        = rl.readline()
+            parsed     = raw_parsing(raw)
+            # R, P, Y    = float(parsed[r]), float(parsed[p]), float(parsed[y])
+            t1 = time.time()
+            elapsed = t1 - t0
+            # print("elapsed", elapsed)
+            R, P       = float(parsed[r]), float(parsed[p])
+            aX, aY, aZ = float(parsed[ax]), float(parsed[ay]), float(parsed[az])
+            if elapsed > time_thr :
+                r_rate = (R - R0)
+                p_rate = (P - P0)
+                # print("rp, %.4f, %.4f, %.4f, %.4f" %(R, P, round(r_rate, 2), round(p_rate, 2)))
+                try :
+                    angle = degrees(atan(p_rate/r_rate))
+                    print("angle", round(angle, 2))
+                except ZeroDivisionError :
+                    if abs(p_rate) > 0 :
+                        angle = 90
+                        # print(angle)
+                # R0, P0 and time Update
+                R0, P0 = R, P
+                t0 = t1
 
-    try :
-        roll0  = 0
-        pitch0 = 0
-        yaw0   = 0
-        moving_avg = 5
-        fname_euler = '/Users/shetshield/Desktop/python_ws/euler_'
-        fname_accel = '/Users/shetshield/Desktop/python_ws/accel_'
-        cnt = 1
-
-        print("press 'i' to start streaming")
-        keyIn = input()
-        while keyIn != 'i' :
-            keyIn = input()
-        print('start')
-
-        for _ in range(moving_avg) :
-            raw_data = serImu.readline()
-            print(raw_data)
-            euler, accel = raw_to_data(raw_data.decode('ascii'))
-            roll0  += euler[0]
-            pitch0 += euler[1]
-            yaw0   += euler[2]
-        roll0  /= float(moving_avg)
-        pitch0 /= float(moving_avg)
-        yaw0   /= float(moving_avg)
-        serImu.reset_input_buffer()
-        time.sleep(0.001)
-        
-        # _st = time.time()
-        arr1 = list()
-        arr2 = list()
-        buffer = b''
-        while True :
-            try :
-                waiting = serImu.in_waiting
-                # print(waiting)
-                if waiting > 0 :
-                    buffer += serImu.read(waiting)
-                    euler, accel = raw_to_data(buffer.decode('ascii'))
-                    arr1.append(euler)
-                    arr2.append(accel)
-                    # print(euler)
-                buffer = b''
-                if len(arr1) > 199 :
-                    # print(time.time()-_st)
-                    np.savetxt(fname_euler + str(cnt) + '.csv', arr1, delimiter=',')
-                    np.savetxt(fname_accel + str(cnt) + '.csv', arr2, delimiter=',')
-                    arr1 = list()
-                    arr2 = list()
-                    print("count", cnt)
-                    cnt += 1
-                    if cnt == 10 :
-                        break
-            except KeyboardInterrupt :
-                break
-            except :
-                pass
-        print("serial close")
-        serImu.close()
-    except :
-        serImu.close()
-        print("\nExit")
+        except KeyboardInterrupt :
+            ser.close()
+            print("\nclose")
+            break
+        except :
+            pass
 
 if __name__=="__main__" :
     main()
