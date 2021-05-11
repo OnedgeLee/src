e34ae8e616a591f4114d61a6c53ebe52f3b05ac7
shetshield <shetshield@gmail.com>
2021-05-12 00:56:09 +0900

    Add files via upload

--- stitching_img/various_thresholding.py
+++ stitching_img/various_thresholding.py
@@ -2,14 +2,26 @@
 # import the necessary packages
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 imagePath = "/Users/shetshield/Desktop/python_ws/processed/10mm_y0mm.jpg"
 
-image = cv2.imread(imagePath, 0)
-(H, W) = image.shape[:2]
+img = cv2.imread(imagePath, 0)
+(H, W) = img.shape[:2]
 
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+img2  = clahe.apply(img)
+
+cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+cv2.namedWindow('img2', cv2.WINDOW_NORMAL)
+cv2.imshow('img', img)
+cv2.imshow('img2', img2)
+cv2.waitKey()
+cv2.destroyAllWindows()
 # convert the image to grayscale, blur it, and perform Canny
 # edge detection
 
+
+"""
 ret,th1 = cv2.threshold(image,127,255,cv2.THRESH_BINARY)
 th2 = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
             cv2.THRESH_BINARY,11,2)
@@ -32,3 +44,4 @@ plt.show()
 # set the blob as the input to the network and perform a forward pass
 # to compute the edges
 print("[INFO] performing holistically-nested edge detection...")
+"""
