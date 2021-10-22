e18ba0b6e1b7014e540bf83f0d0fb4350f7bc27c
shetshield <shetshield@gmail.com>
2021-10-22 16:03:03 +0900

    Add files via upload

--- modular_manipulator/data_gather.py
+++ modular_manipulator/data_gather.py
@@ -50,9 +50,9 @@ def angle_init(_rl, _num) :
             break
     for _ in range(_num) :
         _dat = raw_byte_parsing(_rl.readline())
-        _r += float(_dat[0])
-        _p += float(_dat[1])
-        _y += float(_dat[2])
+        _r += float(_dat[6])
+        _p += float(_dat[7])
+        _y += float(_dat[8])
     _r = round(_r/_num, 4)
     _p = round(_p/_num, 4)
     _y = round(_y/_num, 4)    
@@ -64,25 +64,11 @@ ser = ser_init()
 rl  = ReadLine(ser)
 r0, p0, y0 = angle_init(rl, 10)
 
-
-""" Thread for receiving IMU' data """
-from threading import Thread
-last_received = ''
-def receiving(ser) :
-    global last_received
-    buffer_string = ''
-    while True :
-        buffer_string = buffer_string + ser.read(ser.in_waiting).decode()
-        if '\n' in buffer_string :
-            lines = buffer_string.split('\n')
-            last_received = lines[-2]
-            buffer_string = lines[-1]
-
-
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
-rcl1 = R.from_euler('xz', [-90, 180], degrees = True)
+# rcl1 = R.from_euler('xz', [-90, 180], degrees = True)
+rcl1 = R.from_euler('xy', [-90, 180], degrees = True)
 vo1a  = np.array([0, 68.7, 0])/1000.
 vo1a_ = np.array([0, 48.7, 4])/1000.
 vo1b  = np.array([59.5, -34.4, 0])/1000.
@@ -116,7 +102,6 @@ def rs_calc_dist(_intrinsic, _d_frm, _p) :
     return _pt
 
 def main() :
-    Thread(target=receiving, args=(ser,)).start()
     VEC_INIT = False
     fps      = 30
     A_thr    = 300
@@ -140,10 +125,6 @@ def main() :
         time.sleep(1)
 
         """ cv2 text option """
-        font      = cv2.FONT_HERSHEY_SIMPLEX
-        fontScale = 1
-        t_color   = (0, 255, 0)
-        thickness = 2
         align_to  = rs.stream.color
         align     = rs.align(align_to)
 
@@ -154,103 +135,144 @@ def main() :
         frames = pipe.wait_for_frames()
         a_fs   = align.process(frames)
         d_f    = a_fs.get_depth_frame()
+        c_f    = a_fs.get_color_frame()
         c_intrinsic = d_f.profile.as_video_stream_profile().intrinsics
-        time.sleep(1
+        time.sleep(1)
 
-        while True :
-            frms = pipe.wait_for_frames()
-            a_fs = align.process(frms)
-            d_f  = a_fs.get_depth_frame()
-            c_f  = a_fs.get_color_frame()
-            cimg = np.asanyarray(c_f.get_data())
+        cimg = np.asanyarray(c_f.get_data())
 
-            hsv   = cv2.cvtColor(cimg, cv2.COLOR_BGR2HSV)
-            mask  = cv2.inRange(hsv, lg, ug)
-            c_det = cv2.bitwise_and(cimg, cimg, mask=mask)
-            gray  = cv2.cvtColor(c_det, cv2.COLOR_BGR2GRAY)
-            gray  = cv2.multiply(gray, 10) # Amplifing data
-            ret, img_bin = cv2.threshold(gray, 127, 255, 0)
-            cntrs, hierarchy = cv2.findContours(img_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        hsv   = cv2.cvtColor(cimg, cv2.COLOR_BGR2HSV)
+        mask  = cv2.inRange(hsv, lg, ug)
+        c_det = cv2.bitwise_and(cimg, cimg, mask=mask)
+        gray  = cv2.cvtColor(c_det, cv2.COLOR_BGR2GRAY)
+        gray  = cv2.multiply(gray, 10) # Amplifing data
+        ret, img_bin = cv2.threshold(gray, 127, 255, 0)
+        cntrs, hierarchy = cv2.findContours(img_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-            centroids = list()
-            if len(cntrs) :
-                for cnt in cntrs :
-                    M = cv2.moments(cnt)
-                    A = cv2.contourArea(cnt)
-                    if A > A_thr :
-                        cv2.drawContours(cimg, [cnt], 0, (255, 0, 0), 3)
-                        cx = int(M['m10']/M['m00'])
-                        cy = int(M['m01']/M['m00'])
-                        centroids.append([cx, cy])
-                        if GRAPHICAL_DEBUG :
-                            cv2.circle(cimg, (cx, cy), 3, (0, 0, 255), -1)
-            if len(centroids) == 2 :
-                pt1 = np.round(rs_calc_dist(c_intrinsic, d_f, centroids[0]), 3)
-                pt2 = np.round(rs_calc_dist(c_intrinsic, d_f, centroids[1]), 3)
-                if not VEC_INIT :
-                    if pt2[1] > pt1[1] :
-                        vc_o1 = pt2 - vc_o1a
-                    else :
-                        vc_o1 = pt1 - vc_o1a
-                    if TEXT_DEBUG :
-                        print(pt1, pt2)
-                    vc_a1_ = vc_o1 + vc_o1a_
-                    vc_b1_ = vc_o1 + vc_o1b_
-                    vc_c1_ = vc_o1 + vc_o1c_
+        centroids = list()
+        if len(cntrs) :
+            for cnt in cntrs :
+                M = cv2.moments(cnt)
+                A = cv2.contourArea(cnt)
+                if A > A_thr :
+                    cv2.drawContours(cimg, [cnt], 0, (255, 0, 0), 3)
+                    cx = int(M['m10']/M['m00'])
+                    cy = int(M['m01']/M['m00'])
+                    centroids.append([cx, cy])
+                    if GRAPHICAL_DEBUG :
+                        cv2.circle(cimg, (cx, cy), 3, (0, 0, 255), -1)
+        if len(centroids) == 2 :
+            pt1 = np.round(rs_calc_dist(c_intrinsic, d_f, centroids[0]), 3)
+            pt2 = np.round(rs_calc_dist(c_intrinsic, d_f, centroids[1]), 3)
+            if not VEC_INIT :
+                if pt2[1] > pt1[1] :
+                    vc_o1 = pt2 - vc_o1a
+                else :
+                    vc_o1 = pt1 - vc_o1a
 
-                    # Make DataFrame using Pandas
-                    df = pd.DataFrame(data=np.array([vc_o1]), columns=['x', 'y', 'z'])
-                    da = pd.DataFrame(data=np.array([vc_a1_]), columns=['x', 'y', 'z'])
-                    db = pd.DataFrame(data=np.array([vc_b1_]), columns=['x', 'y', 'z'])    
-                    dc = pd.DataFrame(data=np.array([vc_c1_]), columns=['x', 'y', 'z'])
-                    df = df.append(da)
-                    df = df.append(db)
-                    df = df.append(dc)
-                    df = df.reset_index(drop = True)
+            vc_a1_ = vc_o1 + vc_o1a_
+            vc_b1_ = vc_o1 + vc_o1b_
+            vc_c1_ = vc_o1 + vc_o1c_
 
-                    VEC_INIT = True
-                parsed = raw_string_parsing(last_received)
-                if len(parsed) > 2 :
-                    count += 1
-                    r = float(parsed[0])
-                    p = float(parsed[1])
-                    y = float(parsed[2])
-                    # Rotation Matrix
-                    rl1l2 = R.from_euler('xyz', [r-r0, p-p0, y-y0], degrees=True)
-                    if pt2[1] > pt1[1] :
-                        vc_o2 = pt1 - rcl1.apply(rl1l2.apply(vo2a))
-                    else :
-                        vc_o2 = pt2 - rcl1.apply(rl1l2.apply(vo2a))
-                    vc_a2_ = vc_o2 + rcl1.apply(rl1l2.apply(vo2a_))
-                    vc_b2_ = vc_o2 + rcl1.apply(rl1l2.apply(vo2b_))
-                    vc_c2_ = vc_o2 + rcl1.apply(rl1l2.apply(vo2c_))
+            df = pd.DataFrame(data=np.array([0., 0., 0., 0., 0., 0., vc_o1[0], vc_o1[1], vc_o1[2]]), columns=['v0', 'v1', 'v2', 'v3', 'v6', 'v7', 'x', 'y', 'z'])
+            da = pd.DataFrame(data=np.array([0., 0., 0., 0., 0., 0., vc_a1_[0], vc_a1_[1], vc_a1_[2]]), columns=['v0', 'v1', 'v2', 'v3', 'v6', 'v7', 'x', 'y', 'z'])
+            db = pd.DataFrame(data=np.array([0., 0., 0., 0., 0., 0., vc_b1_[0], vc_b1_[1], vc_b1_[2]]), columns=['v0', 'v1', 'v2', 'v3', 'v6', 'v7', 'x', 'y', 'z'])
+            dc = pd.DataFrame(data=np.array([0., 0., 0., 0., 0., 0., vc_c1_[0], vc_c1_[1], vc_c1_[2]]), columns=['v0', 'v1', 'v2', 'v3', 'v6', 'v7', 'x', 'y', 'z'])
+            df = df.append(da)
+            df = df.append(db)
+            df = df.append(dc)
+            df = df.reset_index(drop = True)
+            print("Check")
+            VEC_INIT = True
 
-                    # Make DataFrame using Pandas
-                    do = pd.DataFrame(data=np.array([vc_o2]), columns=['x', 'y', 'z'])
-                    da = pd.DataFrame(data=np.array([vc_a2_]), columns=['x', 'y', 'z'])
-                    db = pd.DataFrame(data=np.array([vc_b2_]), columns=['x', 'y', 'z'])    
-                    dc = pd.DataFrame(data=np.array([vc_c2_]), columns=['x', 'y', 'z'])
-                    df = df.append(do)
-                    df = df.append(da)
-                    df = df.append(db)
-                    df = df.append(dc)
-                    df = df.reset_index(drop = True)
-                    if count == 200 :
-                        count = 0
-                        step += 1
-                        fname = '/Users/SRBL/Desktop/gusu/srm_data_plot/' + str(step) + '.csv'
-                        df.to_csv(fname, sep=',', na_rep = 'NaN')
-                        dr_list = np.arange(4, len(df))
-                        df = df.drop(dr_list)
-                        df = df.reset_index(drop = True)
-                cv2.imshow('detect', cimg)
-                k = cv2.waitKey(1) & 0xFF
-                if k == 27 :
-                    try :
-                        ser.close()
-                        break
-                    except :
-                        break
+        # End of Vector Initialize
+        if VEC_INIT :
+            while True :
+                if ser.in_waiting :
+                    frms = pipe.wait_for_frames()
+                    a_fs = align.process(frms)
+                    d_f  = a_fs.get_depth_frame()
+                    c_f  = a_fs.get_color_frame()
+                    cimg = np.asanyarray(c_f.get_data())
+    
+                    hsv   = cv2.cvtColor(cimg, cv2.COLOR_BGR2HSV)
+                    mask  = cv2.inRange(hsv, lg, ug)
+                    c_det = cv2.bitwise_and(cimg, cimg, mask=mask)
+                    gray  = cv2.cvtColor(c_det, cv2.COLOR_BGR2GRAY)
+                    gray  = cv2.multiply(gray, 10) # Amplifing data
+                    ret, img_bin = cv2.threshold(gray, 127, 255, 0)
+                    cntrs, hierarchy = cv2.findContours(img_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        
+                    centroids = list()
+                    if len(cntrs) :
+                        for cnt in cntrs :
+                            M = cv2.moments(cnt)
+                            A = cv2.contourArea(cnt)
+                            if A > A_thr :
+                                cv2.drawContours(cimg, [cnt], 0, (255, 0, 0), 3)
+                                cx = int(M['m10']/M['m00'])
+                                cy = int(M['m01']/M['m00'])
+                                centroids.append([cx, cy])
+                                if GRAPHICAL_DEBUG :
+                                    cv2.circle(cimg, (cx, cy), 3, (0, 0, 255), -1)
+                    if len(centroids) == 2 :
+                        pt1 = np.round(rs_calc_dist(c_intrinsic, d_f, centroids[0]), 3)
+                        pt2 = np.round(rs_calc_dist(c_intrinsic, d_f, centroids[1]), 3)
+                        parsed = raw_byte_parsing(rl.readline())
+                        if len(parsed) > 2 :
+                            v0 = float(parsed[0])
+                            v1 = float(parsed[1])
+                            v2 = float(parsed[2])
+                            v3 = float(parsed[3])
+                            v6 = float(parsed[4])
+                            v7 = float(parsed[5])
+                            r = float(parsed[6])
+                            p = float(parsed[7])
+                            y = float(parsed[8])
+                            # Rotation Matrix
+                            rl1l2 = R.from_euler('xyz', [r-r0, p-p0, y-y0], degrees=True)
+                            if pt2[1] > pt1[1] :
+                                vc_o2 = pt1 - rcl1.apply(rl1l2.apply(vo2a))
+                            else :
+                                vc_o2 = pt2 - rcl1.apply(rl1l2.apply(vo2a))
+                            vc_a2_ = vc_o2 + rcl1.apply(rl1l2.apply(vo2a_))
+                            vc_b2_ = vc_o2 + rcl1.apply(rl1l2.apply(vo2b_))
+                            vc_c2_ = vc_o2 + rcl1.apply(rl1l2.apply(vo2c_))
+
+                            # Make DataFrame using Pandas
+                            do = pd.DataFrame(data=np.array([v0, v1, v2, v3, v6, v7, vc_o2[0], vc_o2[1], vc_o2[2]]), columns=['v0', 'v1', 'v2', 'v3', 'v6', 'v7', 'x', 'y', 'z'])
+                            da = pd.DataFrame(data=np.array([v0, v1, v2, v3, v6, v7, vc_a2_[0], vc_a2_[1], vc_a2_[2]]), columns=['v0', 'v1', 'v2', 'v3', 'v6', 'v7', 'x', 'y', 'z'])
+                            db = pd.DataFrame(data=np.array([v0, v1, v2, v3, v6, v7, vc_b2_[0], vc_b2_[1], vc_b2_[2]]), columns=['v0', 'v1', 'v2', 'v3', 'v6', 'v7', 'x', 'y', 'z'])
+                            dc = pd.DataFrame(data=np.array([v0, v1, v2, v3, v6, v7, vc_c2_[0], vc_c2_[1], vc_c2_[2]]), columns=['v0', 'v1', 'v2', 'v3', 'v6', 'v7', 'x', 'y', 'z'])
+                            df = df.append(do)
+                            df = df.append(da)
+                            df = df.append(db)
+                            df = df.append(dc)
+                            df = df.reset_index(drop = True)
+                            fname = '/Users/SRBL/Desktop/gusu/srm_data_plot/' + str(step) + '.csv'
+                            img_f = '/Users/SRBL/Desktop/gusu/srm_data_plot/img/' + str(count) + '.jpg'
+                            cv2.imwrite(img_f, cimg)
+                            df.to_csv(fname, sep=',', na_rep = 'NaN')
+                            count += 1
+                            """
+                            if count == 200 :
+                                count = 0
+                                step += 1
+                                fname = '/Users/SRBL/Desktop/gusu/srm_data_plot/' + str(step) + '.csv'
+                                df.to_csv(fname, sep=',', na_rep = 'NaN')
+                                dr_list = np.arange(4, len(df))
+                                df = df.drop(dr_list)
+                                df = df.reset_index(drop = True)
+                            """
+                        cv2.imshow('detect', cimg)
+                        k = cv2.waitKey(1) & 0xFF
+                        if k == 27 :
+                            try :
+                                ser.close()
+                                cv2.destoryAllWindows()
+                                break
+                            except :
+                                break
             try :
                 ser.close()
                 pipe.stop()
