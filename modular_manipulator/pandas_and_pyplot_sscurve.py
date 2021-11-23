d0247e9145d4e165d09d52219d5ef73d2bef057d
shetshield <shetshield@gmail.com>
2021-11-23 16:43:42 +0900

    stress strain matplotlib

--- modular_manipulator/pandas_and_pyplot_sscurve.py
+++ modular_manipulator/pandas_and_pyplot_sscurve.py
@@ -5,55 +5,53 @@ import numpy as np
 from matplotlib.ticker import MultipleLocator, AutoMinorLocator, FuncFormatter
 from scipy.interpolate import make_interp_spline
 
-df = pd.read_csv('/Users/user/Downloads/srm_data_plot/ss/ss.csv')
 
-df_new = df.rename(columns={'stress_60': '600 min. curing', 'stress_20': '20 min. curing'})
+df = pd.read_csv('/Users/user/Downloads/srm_data_plot/ss/stress_stretch.csv')
 
 plt.rcParams["figure.autolayout"] = True
 # ax1 = df.plot(kind='scatter', x='strain', y='stress_60', marker = '^', color='b', s = 3)
 # ax2 = df.plot(kind='scatter', x='strain', y='stress_20', ax=ax1, color='r', s = 3)
 
-ax1 = df_new.plot.line(x='strain', y='600 min. curing', c='black')
-ax2 = df_new.plot.line(x='strain', y='20 min. curing', ax=ax1, ls='--', c='black')
-# ax1.get_legend().remove()
-ax1.legend(edgecolor='black')
-ax1.xaxis.set_minor_locator(AutoMinorLocator())
-ax1.yaxis.set_minor_locator(AutoMinorLocator())
+ax = df.plot.line(x='stretch', y='stress', c='black')
+# ax = df.plot.line(x='strain', y='20 min. curing', ax=ax1, ls='--', c='black')
+ax.get_legend().remove()
+ax.xaxis.set_minor_locator(AutoMinorLocator())
+ax.yaxis.set_minor_locator(AutoMinorLocator())
 
 # Add comma to Tick
 # ax.get_xaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
 
-ax1.set_xlabel('Strain \u03B5')
-ax1.set_ylabel('Stress \u03C3 (kPa)')
+# ax.set_xlabel('Stretch \u03B5')
+ax.set_xlabel('Stretch \u03BB', fontsize=16)
+ax.set_ylabel('Stress \u03C3 (MPa)', fontsize=16)
 
-"""
-X1 = list()
-X2 = list()
-Y1 = list()
-Y2 = list()
-for val in df.values :
-    _x = val[0]
-    if not np.isnan(val[1]) :
-        X1.append(_x)
-        _y1 = val[1]
-    if not np.isnan(val[2]) :
-        X2.append(_x)
-        _y2 = val[2]
-    Y1.append(_y1)
-    Y2.append(_y2)
+ax.tick_params(which='both', width=1)
+ax.tick_params(which='major', length=7)
+ax.tick_params(which='minor', length=3, color='black')
 
-X1 = np.array(X1)
-X2 = np.array(X2)
-Y1 = np.array(Y1)
-Y2 = np.array(Y2)
+x_tick = [1.0, 2.0, 3.0, 4.0]
+ax.set_xticks(x_tick)
+y_tick = [0.0, 0.8, 1.6, 2.4]
+ax.set_yticks(y_tick)
 
-xs=np.linspace(0,0.8,500)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
 
-model1 = make_interp_spline(X1, Y1)
-model2 = make_interp_spline(X2, Y2)
 
-y1 = model1(xs)
-y2 = model2(xs)
+a = 0.3342
+b = -0.1178
+# eq = 2*(1-x**(-3))*(x*a+b)
 
-plt.scatter(X1, y1)
-"""
+X = np.linspace(1, 4, 301)
+Y = list()
+for _x in X :
+    y = 2*(1-pow(_x, -3))*(_x*a+b)
+    Y.append(y)
+
+ax.plot(X, Y, c='blue')
+
+# ax.legend(edgecolor='black')
+ax.legend(['Test Result', 'Fitted Curve'], edgecolor='black', fontsize=14)
+
+eq = r'$\sigma = 2\cdot(1- \lambda^{-3})\cdot(C_{01}\cdot\lambda+C_{10})$'
+plt.text(2.1, 0.3, eq, fontsize=15)
